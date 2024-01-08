e66898b4762f0760cd9a97a205aea9b71118729f
Rahees Ahmed <raheesahmed256@gmail.com>
2024-01-09 02:52:33 +0500

    Added: save user details to CSV file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import OpenAI from "openai";
-
+import fs from "fs";
 const apiKey = process.env.OPENAI_API_KEY;
 
 dotenv.config();
@@ -18,16 +18,20 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const generateCsv = (data) => {
-  const csvData = data.map((row) => {
-    return row.join(",");
+//get the username and email from the /generate-images and save it into the userdsDetails.csv file in the public folder
+const savetocsv = (firstName, email) => {
+  const data = `${firstName},${email}\n`;
+  fs.appendFile("public/userDetails.csv", data, (err) => {
+    if (err) throw err;
+    console.log("Data added to file");
   });
-  return csvData.join("\n");
 };
 
 app.post("/generate-images", upload.none(), async (req, res) => {
   try {
     console.log("Received data:", req.body);
+    const { firstName, email } = req.body;
+    savetocsv(firstName, email);
 
     const {
       mainScene,
